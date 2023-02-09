a6ec6eebb7e0595be165dca2b5c2d392ea6ec255
Behailu Weldeyohannes <bweldeyohannes@gmail.com>
2023-02-08 23:11:44 -0500

    updated

--- 01-conditionals/main.js
+++ 01-conditionals/main.js
@@ -55,6 +55,7 @@ else if(time === "evening"){
 }
 
 */
+/*
 // time switch
 const time = "morning"
 switch (time) {
@@ -70,3 +71,76 @@ switch (time) {
   default:
     console.log("Good night!");
 }
+*/
+/*
+//  shorten conditionals with ternaries
+const isAuthenticated = false;
+
+// let cartItemCount = 0;
+
+// if (isAuthenticated) {
+//   // add item to cart
+//   cartItemCount = 1;
+// } else {
+//   // tell user to login
+// //   console.log("Please log in!");
+//   cartItemCount = 0;
+// }
+
+const cartItemCount = isAuthenticated ? 1 : 0;
+console.log(cartItemCount);
+*/
+/*
+const age = 20;
+
+if (age < 10) {
+  greeting = "Hey there";
+} else if (age > 18) {
+  greeting = "Greetings";
+} else if (age > 10) {
+  greeting = "What's up?";
+} else {
+  greeting = "That's an interesting age!";
+}
+console.log(greeting);
+*/
+// const age = 20;
+// const greeting = age < 10 ? "Hey, there" : "That's an interesting age!";
+// console.log(greeting)
+// const age = 11;
+// const greeting = age < 10 ? "Hey, there" : age > 18 ? "Greeting" : age > 10 ? "What's up" : "That's an interesting age!"
+// console.log(greeting)
+/*
+
+// Challenge 1: Check if the user is allowed to add an "iPad" to its shopping cart. Write a ternary that sets the shoppingCart variable to "iPad" if the user is authenticated, and an empty string if the user isn't authenticated
+
+const isAuthenticated = true;
+
+shoppingCart = isAuthenticated ? "ipad" : ""
+console.log("shoppingCart: ", shoppingCart);
+
+// Challenge 2: Check if user is old enough. Write a ternary that sets allowedAccess to true if the age is 18 or more, or false if 
+
+const age = 18;
+let allowedAccess;
+
+allowedAccess = age > 17 ? true : false
+console.log("allowedAccess: ", allowedAccess);
+*/
+
+// short-circuting
+const response = "";
+
+// let username;
+
+// if (response) {
+//   username = response;
+// } else {
+//   username = "guest";
+// }
+
+// console.log(username);
+
+// const username = response ? response : "guest"
+const username = response || "guest"
+console.log(username)
