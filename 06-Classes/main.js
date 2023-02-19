9521ec781eb296b0d56bc01ea4c13474251d7d2a
Behailu Weldeyohannes <bweldeyohannes@gmail.com>
2023-02-19 10:43:14 -0500

    classes

--- 06-Classes/main.js
+++ 06-Classes/main.js
@@ -191,20 +191,21 @@ class Product {
   constructor(name, price) {
     this.name = name;
     this.price = price;
+    this.favoriteProduct = this.favoriteProduct.bind(this);
   }
 
-  handleFavoriteProduct() {
+  handleFavoriteProduct = () => {
     if (isAuth) {
-      this.favoriteProduct();
+      setTimeout(this.favoriteProduct, 1000);
     } else {
       console.log("You must be signed in to favorite products!");
     }
-  }
+  };
 
-  favoriteProduct() {
+  favoriteProduct = () => {
     user.favorites.push(this.name);
     console.log(`${this.name} favorited!`);
-  }
+  };
 }
 
 const product1 = new Product("Coaster", 89.99);
